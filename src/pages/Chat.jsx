e617cf54e5cfdd11684c3d8054efc9423e8d95ea
skytheakash29 <145717209+skytheakash29@users.noreply.github.com>
2023-09-22 12:07:10 +0600

    Update Chat.jsx

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,4 +1,4 @@
-import logo from '../assets/logo.png';
+import logo from '../assets/Logo.png';
 import { Link } from 'react-router-dom';
 import { useMutation } from 'react-query';
 import {useState,React,Fragment} from 'react'
